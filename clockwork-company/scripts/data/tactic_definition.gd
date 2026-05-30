3e001fd8393a5b564b958b16c12e0f03afad68f8
Alastair Quincy Van Maren <aqvanmaren@gmail.com>
2026-05-30 15:23:47 -0600

    Implement phase 5 basic tactics

---
+++ clockwork-company/scripts/data/tactic_definition.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+class_name TacticDefinition
+
+@export_enum("Always", "Self HP Below Half", "Ally HP Below Half", "Enemy Alive") var condition := "Always"
+@export_enum("Attack", "Heal", "Guard") var action := "Attack"
+@export_enum("Self", "Lowest HP Ally", "Frontmost Enemy") var target := "Frontmost Enemy"
